d45ee3c0ea247f1a74b7e5d4781e32904a73f157
Jovan Ho <jovan_ho@tech.gov.sg>
2021-02-22 21:54:52 +0800

    set challenge endpoint for dropbox webhook verification test

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ app.get('/hi', (req, res) => {
   res.status(200).send({ msg: 'hi from api' });
 });
 
+app.get('/test', (req, res) => {
+  challengeResponse = req.query.challenge;
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.status(200).send(challengeResponse);
+});
+
 app.post('/test', (req, res) => {
   console.log(req.body);
   res.status(200).send({
     msg: 'webhook received successfully',
     data: req.body
   })
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Express server started on port ${PORT}`)
