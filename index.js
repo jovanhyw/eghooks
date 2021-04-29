b56e386815576c6de80797e922d0cc918949b4c7
Jovan Ho <jovan_ho@tech.gov.sg>
2021-04-29 11:02:00 +0800

    update webhook endpoint to handle timestamping

--- index.js
+++ index.js
@@ -20,18 +20,45 @@ app.get('/webhook', (req, res) => {
   res.status(200).send({ 'challengeToken': challengeResponse });
 });
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', async (req, res) => {
   console.log(req.body);
   console.log(req.headers);
 
-  signature = req.headers['x-techpass-signature'];
+  await new Promise(r => setTimeout(r, 2000));
+
+  // 1. Save the current timestamp of the received request to a variable
+  timeReceived = Math.floor(new Date().getTime() / 1000)
+  console.log(timeReceived);
   secret = process.env.WEBHOOK_SECRET;
 
-  // compute and verify hmac hash
-  computed = crypto.createHmac("sha256", secret).update(JSON.stringify(req.body)).digest('hex');
-  valid = signature === computed
+  // 2. From the X-TECHPASS-SIGNATURE header, extract the timestamp and signatures
+  signature = req.headers['x-techpass-signature'];
+  const [timekv, signedKv] = signature.split(',')
+  const [, timeSent] = timekv.split('=')
+  console.log(timeSent);
+  const [, signedHash] = signedKv.split('=')
+  console.log(signedHash);
 
-  console.log(computed);
+  console.log('time difference:', timeReceived - timeSent);
+  console.log('signedHash:', signedHash);
+
+  // If more than 5 mins, treat it as a replay attack and ignore request
+  if (timeReceived - timeSent > (60 * 5)) {
+    console.log('replay atk');
+    return res.status(200).send()
+  }
+
+  // 3. Save the raw JSON payload to a variable
+  reqPayload = JSON.stringify(req.body);
+  // 4. Prepare the signature payload by concatenating the timestamp and JSON payload together, using a colon(:) as a delimiter.
+  signedPayload = `${timeSent}:${reqPayload}`
+  // 5. Compute a SHA256 HMAC of the signature payload by using your webhook secret as the key.
+  // 6. Compute the hex digest of the HMAC.
+  computed = crypto.createHmac("sha256", secret).update(signedPayload).digest('hex');
+  // 7. Compare your computed signature against the signature extracted from
+  valid = signedHash === computed
+
+  console.log('computed:', computed);
   console.log('verified:', valid);
 
   if (valid) {
